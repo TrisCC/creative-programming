f9012375a5fd948b015b4fb520e789fd524a6104
TrisCC <tristancotino@gmail.com>
2025-09-18 09:51:14 +0200

    Add growing rings

--- assignments/assignment2.js
+++ assignments/assignment2.js
@@ -1,22 +1,24 @@
 let timeVal = 0;
 let centerX = 0;
 let centerY = 0;
+let recursionDepth = 4;
+let rings = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
   centerX = width / 2;
   centerY = height / 2;
+  rings.push(new Ring(centerX, centerY, 0));
+  rings.push(new Ring(centerX, centerY, 600));
 }
 
 function draw() {
   background(0);
-  // fill(255);
-  stroke(255);
-  noFill();
-  let circleRadius = timeVal % (max(width, height) + 500);
-  ellipse(centerX, centerY, circleRadius, circleRadius);
-  timeVal += 10;
+  rings.forEach((ring) => {
+    ring.update();
+    ring.draw();
+  });
 }
 
 // Set the center coordinates when the window is resized
@@ -24,4 +26,32 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   centerX = width / 2;
   centerY = height / 2;
+
+  // Update ring positions
+  rings.forEach((ring) => {
+    ring.x = centerX;
+    ring.y = centerY;
+  });
+}
+
+class Ring {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+
+  update() {
+    this.radius += 1;
+    if (this.radius > 1200) {
+      this.radius = 0;
+    }
+  }
+
+  draw() {
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    circle(this.x, this.y, this.radius);
+  }
 }
