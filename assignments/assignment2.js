e1ab5e25eb75891e5479b1ca1c6e960d4739c0a5
TrisCC <tristancotino@gmail.com>
2025-09-22 16:22:53 +0200

    ass 2

--- assignments/assignment2.js
+++ assignments/assignment2.js
@@ -10,60 +10,43 @@ function setup() {
 
   translate(centerX, centerY);
   push();
-
-  drawTree(0, 0, height / 4, layersTotal);
 }
 
 function draw() {
-  // background(0);
+  background(0);
+  translate(centerX, centerY);
+  drawCircles(layersTotal, color(60, 125, 40), color(150, 100, 25));
+  drawCircles(layersTotal, color(150, 100, 25), color(60, 125, 40));
+  noLoop();
 }
 
-function drawTree(x, y, len, layers) {
+function drawCircles(layer, c1, c2) {
   // Exit condition
-  if (layers == 0 || len < 2) {
+  if (layer < 0) {
     return;
   }
 
   // Set color
   strokeWeight(2);
-  fill(0);
-  let strokeColor = lerpColor(
-    color(45, 110, 25),
-    color(135, 85, 10),
-    layers / layersTotal
-  );
+  let strokeColor = lerpColor(c1, c2, layer / layersTotal);
+  fill(0, 0, 0, 0);
+
   stroke(strokeColor);
 
-  // Determine if trunk or branches need to be drawn
-  if (layers > layersTotal * 0.6) {
-    let angle = random(-10, 10);
+  ellipse(random(-12, 12), random(-5, 5), layer * 15, layer * 15);
 
-    // Draw the tree
-    rotate(radians(angle));
-    translate(0, -2);
-    push();
-    ellipse(x, y, 20 * (layers / layersTotal), 10);
-
-    // recursive call
-    drawTree(x, y - 2, 50, layers - 1);
-  } else {
-    let angle = random(-5, 5);
-
-    // Draw the tree
-    rotate(radians(angle));
-    translate(0, -2);
-    push();
-    ellipse(x, y, 20 * (layers / layersTotal), 10);
-
-    // recursive call
-    drawTree(x, y - 2, 50, layers - 1);
-  }
-
-  pop();
+  drawCircles(layer - random(2, 6), c1, c2);
 }
 
 // Set the center coordinates when the window is resized
 function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  centerX = width / 2;
+  centerY = height / 2;
+}
+
+// Generate new pattern on mouse press
+function mousePressed() {
   resizeCanvas(windowWidth, windowHeight);
   centerX = width / 2;
   centerY = height / 2;
