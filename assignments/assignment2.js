e486ca5c90f62d95babefa784a29cf8ac8275250
TrisCC <tristancotino@gmail.com>
2025-09-22 15:37:14 +0200

    Stuff

--- assignments/assignment2.js
+++ assignments/assignment2.js
@@ -1,24 +1,65 @@
-let timeVal = 0;
 let centerX = 0;
 let centerY = 0;
-let recursionDepth = 4;
-let rings = [];
+let layersTotal = 100;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
+  background(0);
   centerX = width / 2;
   centerY = height / 2;
-  rings.push(new Ring(centerX, centerY, 0));
-  rings.push(new Ring(centerX, centerY, 600));
+
+  translate(centerX, centerY);
+  push();
+
+  drawTree(0, 0, height / 4, layersTotal);
 }
 
 function draw() {
-  background(0);
-  rings.forEach((ring) => {
-    ring.update();
-    ring.draw();
-  });
+  // background(0);
+}
+
+function drawTree(x, y, len, layers) {
+  // Exit condition
+  if (layers == 0 || len < 2) {
+    return;
+  }
+
+  // Set color
+  strokeWeight(2);
+  fill(0);
+  let strokeColor = lerpColor(
+    color(45, 110, 25),
+    color(135, 85, 10),
+    layers / layersTotal
+  );
+  stroke(strokeColor);
+
+  // Determine if trunk or branches need to be drawn
+  if (layers > layersTotal * 0.6) {
+    let angle = random(-10, 10);
+
+    // Draw the tree
+    rotate(radians(angle));
+    translate(0, -2);
+    push();
+    ellipse(x, y, 20 * (layers / layersTotal), 10);
+
+    // recursive call
+    drawTree(x, y - 2, 50, layers - 1);
+  } else {
+    let angle = random(-5, 5);
+
+    // Draw the tree
+    rotate(radians(angle));
+    translate(0, -2);
+    push();
+    ellipse(x, y, 20 * (layers / layersTotal), 10);
+
+    // recursive call
+    drawTree(x, y - 2, 50, layers - 1);
+  }
+
+  pop();
 }
 
 // Set the center coordinates when the window is resized
@@ -26,32 +67,4 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   centerX = width / 2;
   centerY = height / 2;
-
-  // Update ring positions
-  rings.forEach((ring) => {
-    ring.x = centerX;
-    ring.y = centerY;
-  });
-}
-
-class Ring {
-  constructor(x, y, radius) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-  }
-
-  update() {
-    this.radius += 1;
-    if (this.radius > 1200) {
-      this.radius = 0;
-    }
-  }
-
-  draw() {
-    stroke(255);
-    strokeWeight(4);
-    noFill();
-    circle(this.x, this.y, this.radius);
-  }
 }
