1f3de0ed0ef7d4871b0e4572728739f8773d23a7
TrisCC <tristancotino@gmail.com>
2025-10-06 14:41:01 +0200

    Finish assignment 4

--- assignments/assignment4.js
+++ assignments/assignment4.js
@@ -7,8 +7,10 @@ let planets = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  emitters.push(new Emitter(width / 2 - 200, height / 2));
-  emitters.push(new Emitter(width / 2 + 200, height / 2));
+
+  // Initialize basic objects
+  emitters.push(new Emitter(width / 2 - 300, height / 2));
+  emitters.push(new Emitter(width / 2 + 300, height / 2));
   planets.push(new Planet(width / 2 - 50, height / 2 - 100, 30));
   planets.push(new Planet(width / 2 + 50, height / 2 + 100, 30));
 }
@@ -16,16 +18,17 @@ function setup() {
 function draw() {
   background(0);
 
+  // Emitter loop
   for (let emitter of emitters) {
     if (frameCount % 20 === 0) {
       emitter.addParticle();
     }
     emitter.run();
-    emitter.show();
   }
 
+  // Planet loop
   for (let planet of planets) {
-    planet.show();
+    planet.run();
   }
 }
 
@@ -33,6 +36,11 @@ class Emitter {
   constructor(x, y) {
     this.origin = createVector(x, y);
     this.particles = [];
+  }
+
+  run() {
+    this.show();
+    this.update();
   }
 
   addParticle() {
@@ -45,7 +53,8 @@ class Emitter {
     circle(this.origin.x, this.origin.y, 8);
   }
 
-  run() {
+  update() {
+    // Update and remove dead particles
     for (let i = this.particles.length - 1; i >= 0; i--) {
       let particle = this.particles[i];
       particle.run();
@@ -65,23 +74,34 @@ class Particle {
   }
 
   run() {
-    this.update();
     this.show();
-  }
-
-  update() {
-    this.applyForce(); // Gravity
-    this.velocity.add(this.acceleration);
-    this.position.add(this.velocity);
-    this.acceleration.mult(0);
-    this.lifespan -= 2;
+    this.update();
   }
 
   show() {
     stroke(0, this.lifespan);
     strokeWeight(2);
     fill(255, this.lifespan);
-    circle(this.position.x, this.position.y, 8);
+    circle(this.position.x, this.position.y, 12);
+  }
+
+  update() {
+    this.applyForce();
+
+    this.velocity.add(this.acceleration);
+    // Check for collisions with planets
+    for (let planet of planets) {
+      if (this.checkCollision(planet)) {
+        // Calculate reflection vector
+        let normal = p5.Vector.sub(this.position, planet.position).normalize();
+        let dot = this.velocity.dot(normal);
+        let reflection = p5.Vector.sub(this.velocity, p5.Vector.mult(normal, 2 * dot));
+        this.velocity = reflection;
+      }
+    }
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+    this.lifespan -= 2;
   }
 
   isDead() {
@@ -96,6 +116,11 @@ class Particle {
     }
     this.acceleration.add(force);
   }
+
+  checkCollision(planet) {
+    let distance = p5.Vector.dist(planet.position, this.position);
+    return distance < planet.radius;
+  }
 }
 
 class Planet {
@@ -103,30 +128,63 @@ class Planet {
     this.position = createVector(x, y);
     this.mass = mass;
     this.radius = mass; // Simplified radius based on mass
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+
+    // Set initial velocity
+    if (this.position.y <= height / 2 - 100) {
+      this.applyForce(createVector(0, 0.3));
+    } else if (this.position.y >= height / 2 + 100) {
+      this.applyForce(createVector(0, -0.3));
+    }
   }
 
   attract(particle) {
     let force = p5.Vector.sub(this.position, particle.position);
     let distance = constrain(force.mag(), 5, 25);
     force.normalize();
-    let strength = (0.4 * this.mass * 3) / (distance * distance); // Assuming particle mass is 1
+    // Gravitational constant is 0.4 and mass is 3 (for no particular reason)
+    let strength = (0.4 * this.mass * 3) / (distance * distance);
     force.mult(strength);
     return force;
   }
 
+  run() {
+    this.show();
+    this.update();
+  }
+
   show() {
-    fill(100, 150, 250);
+    fill(51, 204, 51);
     noStroke();
     circle(this.position.x, this.position.y, this.radius * 2);
+  }
+
+  update() {
+    // Move planets up and down
+    if (this.position.y <= height / 2 - 100) {
+      this.applyForce(createVector(0, 0.3));
+    } else if (this.position.y >= height / 2 + 100) {
+      this.applyForce(createVector(0, -0.3));
+    } else {
+      if (this.velocity.mag() > 2) {
+        this.velocity.mult(1);
+      } else {
+        this.velocity.mult(1.05);
+      }
+    }
+
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+  }
+
+  applyForce(force) {
+    this.acceleration.add(force);
   }
 }
 
 function mousePressed() {
+  // Add a new emitter at mouse position
   emitters.push(new Emitter(mouseX, mouseY));
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  centerX = width / 2;
-  centerY = height / 2;
 }
