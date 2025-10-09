6fc768609cef9357e57464d6e196a529cf890e70
TrisCC <tristancotino@gmail.com>
2025-10-09 09:14:09 +0200

    Init asignment 5

---
+++ assignments/assignment5.js
@@ -0,0 +1,12 @@
+/// <reference path="../libraries/p5.global-mode.d.ts" />
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function draw() {
+  background(0);
+}
+
+function mousePressed() {}
