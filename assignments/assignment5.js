bcfc83217f1cb72402008271dac93e1d215ccb08
TrisCC <tristancotino@gmail.com>
2025-10-09 09:24:19 +0200

    Basic shape

--- assignments/assignment5.js
+++ assignments/assignment5.js
@@ -1,5 +1,7 @@
 /// <reference path="../libraries/p5.global-mode.d.ts" />
 
+let points = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
@@ -7,6 +9,35 @@ function setup() {
 
 function draw() {
   background(0);
+  for (let p of points) {
+    fill(255);
+    noStroke();
+    circle(p.x, p.y, 8);
+  }
+
+  for (let i = 0; i < points.length - 1; i++) {
+    stroke(255);
+    line(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+  }
+  if (points.length > 2) {
+    line(
+      points[0].x,
+      points[0].y,
+      points[points.length - 1].x,
+      points[points.length - 1].y
+    );
+  }
 }
 
-function mousePressed() {}
+function mousePressed() {
+  points.push(createVector(mouseX, mouseY));
+  if (points.length > 5) {
+    points.shift();
+  }
+  if (points.length > 1) {
+    for (let i = 0; i < points.length - 1; i++) {
+      stroke(255);
+      line(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+    }
+  }
+}
